bdd862e579104b6f44a898655e9562a6d233e8bb
ApolloTeddy <avzaviercallen@gmail.com>
2022-05-16 10:04:32 -0400

    Pulsey thing

--- Fourier Transform/main.cpp
+++ Fourier Transform/main.cpp
@@ -5,29 +5,28 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-#include <stdio.h>
-
 #define TAU (M_PI*2)
 #define mapX(a) (a*2/wid - 1)
 #define mapY(a) -(a*2/hei - 1)
 
-const unsigned int dWid = 800, dHei = 800, minCircleQuality = 10, maxCircleQuality = 50, VERTICES = 50;
+const unsigned int wid = 800, hei = 800, minCircleQuality = 10, maxCircleQuality = 50, VERTICES = 50;
 
 double mX, mY;
-int wid, hei;
+
+
 const char *vertShaderSource = "#version 330 core\n"
 						       "layout (location = 0) in vec3 aPos;\n"
 							   "void main() { gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); }",
 		   *fragShaderSource = "#version 330 core\n"
 							   "out vec4 FragColor;\n"
-						       "void main() { FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); }";
+						       "void main() { FragColor = vec4(0.0f, 0.0f, 0.0f, 0.0f); }";
 
 void circle(double* arr, double x, double y, double r) {
 	// unsigned int VERTICES = r/10 + 10; // when r = 400, VERTICES = maxCircleQuality, when r = 0, VERTICES = minCircleQuality. 10 + r/10
 	// arr = new double[VERTICES * 3];
 
-	if (wid == 0) r *= (2.0 / dWid);
-	else r *= (2.0 / wid);
+	r *= (2.0 / wid);
+
 	for (int v = 0; v <= VERTICES; v++) {
 		double t = v * TAU / VERTICES;
 
@@ -56,7 +55,7 @@ void framebuffersizeCallback(GLFWwindow* win, int widt, int heig) {
 }
 
 void init() {
-	glViewport(0, 0, dWid, dHei);
+	glViewport(0, 0, wid, hei);
 }
 
 int main(void) {
@@ -69,7 +68,7 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	win = glfwCreateWindow(dWid, dHei, "Fourier Transform.", NULL, NULL); 
+	win = glfwCreateWindow(wid, hei, "Fourier Transform.", NULL, NULL); 
 	glfwMakeContextCurrent(win);
 
 	glfwSetFramebufferSizeCallback(win, framebuffersizeCallback);
@@ -79,10 +78,10 @@ int main(void) {
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) exit(EXIT_FAILURE);
 
-	unsigned int vertShader = glCreateShader(GL_VERTEX_SHADER),
-				 fragShader = glCreateShader(GL_FRAGMENT_SHADER),
-				 shaderProgram = glCreateProgram(),
-   	             VBO, VAO;
+	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER),
+		   fragShader = glCreateShader(GL_FRAGMENT_SHADER),
+		   shaderProgram = glCreateProgram(),
+   	       VBO, VAO;
 
 	glShaderSource(vertShader, 1, &vertShaderSource, NULL);
 	glShaderSource(fragShader, 1, &fragShaderSource, NULL);
@@ -104,10 +103,8 @@ int main(void) {
 
 	glBindVertexArray(VAO);
 
-	circle(cVert, mX, mY, 10);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cVert), cVert, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 150, 0, GL_DYNAMIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 3 * sizeof(double), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -116,13 +113,12 @@ int main(void) {
 
 	init();
 
+	double r = 10;
 	while (!glfwWindowShouldClose(win)) {
-		glfwGetFramebufferSize(win, &wid, &hei);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		circle(cVert, mX, mY, 10);
+		circle(cVert, mX, mY, r);
 
 		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cVert), cVert);
 
@@ -133,7 +129,10 @@ int main(void) {
 
 		glfwSwapBuffers(win);
 		glfwPollEvents();
-	}
+
+		r += 0.025;
+		if (r > 60) r = 10;
+ 	}
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
